bb4597079f074a7624ea4d16da46f9abc623c36e
Dan <decisivedaniel@proton.me>
2025-05-26 01:11:33 +0200

    Add get last updated to handle updates

--- Services/IShowService.cs
+++ Services/IShowService.cs
@@ -6,6 +6,7 @@ interface IShowService
 {
     Task<List<Show>> GetAllAsync();
     Task<List<Show>> GetPageAsync(int pageNumber = 0, int pageSize = 250);
+    Task<long> GetLastUpdatedValue();
     Task CreateAsync(Show newShow);
     Task UpdateAsync(Show updatedShow);
 }
