aa9dbf42a46cb94c25d8fbf557e687e67d1cc7e4
Dan <decisivedaniel@proton.me>
2025-05-26 01:12:55 +0200

    Naviagtion for many to many link

--- Models/Actor.cs
+++ Models/Actor.cs
@@ -5,4 +5,6 @@ public class Actor
     public int Id { get; set; }
     public string Name { get; set; }
     public DateOnly? Birthday { get; set; }
+    public long LastUpdated { get; set; }
+    public ICollection<ShowActor> ShowActors { get; set; }
 }
--- Models/Show.cs
+++ Models/Show.cs
@@ -4,5 +4,6 @@ public class Show
 {
     public int Id { get; set; }
     public string Title { get; set; }
+    public long LastUpdated { get; set; }
     public ICollection<ShowActor> ShowActors { get; set; }
 }
