5f7ba05d0784093f90c9ce901096069892d219d1
Dan <decisivedaniel@proton.me>
2025-05-27 00:28:43 +0200

    Fully testing GetLastUpdatedValue method

--- TVMazeScraper.Tests/ServicesTests/SqliteShowServiceTests.cs
+++ TVMazeScraper.Tests/ServicesTests/SqliteShowServiceTests.cs
@@ -1,29 +1,70 @@
-using TVMazeScraper.Services;
+using Moq;
+using Moq.EntityFrameworkCore;
 using TVMazeScraper.Data;
-using Microsoft.EntityFrameworkCore;
+using TVMazeScraper.Models;
 
 namespace TVMazeScraper.Services.Tests;
 
 public class SqliteShowServiceTests
 {
     [Fact]
-    public void GetLastUpdatedValue_ShouldReturnNull_IfEmpty()
+    public async Task GetLastUpdatedValue_ShouldReturnZero_IfEmpty()
     {
         //Arrange
-        var context = new ProgramContext(new DbContextOptionsBuilder<ProgramContext>().Options);
-        var service = new SqliteShowService(context);
+        var data = new List<Show>().AsQueryable();
+        var mockContext = new Mock<ProgramContext>();
+        mockContext.Setup(c => c.Shows).ReturnsDbSet(data);
+        var service = new SqliteShowService(mockContext.Object);
 
         //Act
-        var data = service.GetLastUpdatedValue();
+        var value = await service.GetLastUpdatedValue();
 
         //Assert
-        Assert.Null(data);
+        Assert.Equal(0, value);
     }
 
     [Fact]
-    public void GetLastUpdatedValue_ShouldReturnHighest_IfValueExists()
+    public async Task GetLastUpdatedValue_ShouldReturnHighest_IfOneShowExists()
     {
-        
+        var updated = 200;
+        var show = new Show()
+        {
+            Id = 1,
+            LastUpdated = updated
+        };
+        var data = new List<Show>().Append(show);
+        var mockContext = new Mock<ProgramContext>();
+        mockContext.Setup(c => c.Shows).ReturnsDbSet(data);
+        var service = new SqliteShowService(mockContext.Object);
+
+        //Act
+        var value = await service.GetLastUpdatedValue();
+
+        Assert.Equal(updated, value);
+    }
+
+    [Fact]
+    public async Task GetLastUpdatedValue_ShouldReturnHighest_IfMultipleShowsExists()
+    {
+        var updated = 200;
+        var show1 = new Show()
+        {
+            LastUpdated = updated
+        };
+        updated++;
+        var show2 = new Show()
+        {
+            LastUpdated = updated
+        };
+        var data = new List<Show> { show1, show2 };
+        var mockContext = new Mock<ProgramContext>();
+        mockContext.Setup(c => c.Shows).ReturnsDbSet(data);
+        var service = new SqliteShowService(mockContext.Object);
+
+        //Act
+        var value = await service.GetLastUpdatedValue();
+
+        Assert.Equal(updated, value);
     }
 }
 
