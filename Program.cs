a16bdda4f4949c4d2bb5b77776d46ea826672e69
Dan <decisivedaniel@proton.me>
2025-05-26 02:09:23 +0200

    Using sqlite

--- Program.cs
+++ Program.cs
@@ -1,3 +1,7 @@
+using Microsoft.EntityFrameworkCore;
+using TVMazeScraper.Data;
+using TVMazeScraper.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +10,20 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<ProgramContext>((serviceProvider, options) =>
+    {
+        var environment = serviceProvider.GetRequiredService<IHostEnvironment>();
+        var dataDirectory = environment.ContentRootPath + "/App_Data";
+        if (!Directory.Exists(dataDirectory))
+        {
+            Directory.CreateDirectory(dataDirectory);
+        }
+        var databaseFile = dataDirectory + ".db";
+        options.UseSqlite("Data Source=" + databaseFile);
+    });
+
+builder.Services.AddScoped<IShowService, SqliteShowService>();
 
 var app = builder.Build();
 
