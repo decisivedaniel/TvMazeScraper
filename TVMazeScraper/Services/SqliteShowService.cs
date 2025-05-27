39c301cf695e2142acc175dd9fe7581010683ecd
Dan <decisivedaniel@proton.me>
2025-05-27 20:53:43 +0200

    Update show service with inefficient method

--- TVMazeScraper/Services/SqliteShowService.cs
+++ TVMazeScraper/Services/SqliteShowService.cs
@@ -1,4 +1,5 @@
 using Microsoft.EntityFrameworkCore;
+using SQLitePCL;
 using TVMazeScraper.Data;
 using TVMazeScraper.Models;
 
@@ -7,28 +8,62 @@ namespace TVMazeScraper.Services;
 public class SqliteShowService : IShowService
 {
     private readonly ProgramContext _context;
+    private readonly ILogger<SqliteShowService> _logger;
 
-    public SqliteShowService(ProgramContext programContext)
+    public SqliteShowService(ProgramContext programContext, ILogger<SqliteShowService> logger)
     {
         _context = programContext;
+        _logger = logger;
     }
 
-    public Task CreateAsync(Show newShow)
+    public async Task CreateOrUpdateAsync(Show show, List<Actor> actors)
     {
-        throw new NotImplementedException();
+        actors.ForEach(actor =>
+        {
+            var existingActor = _context.Actors.SingleOrDefault(a => a.Id == actor.Id);
+            _ = existingActor == null ? _context.Actors.Add(actor) : _context.Actors.Update(actor);
+        });
+        await _context.SaveChangesAsync();
+        var existingShow = await _context.Shows.SingleOrDefaultAsync(s => s.Id == show.Id);
+        _ = existingShow == null ? _context.Shows.Add(show) : _context.Shows.Update(show);
+        await _context.SaveChangesAsync();
+        existingShow = await _context.Shows.SingleAsync(s => s.Id == show.Id);
+        actors.ForEach(actor =>
+        {
+            existingShow.ShowRoles.Add(new Role
+            {
+                Show = existingShow,
+                Actor = actor
+            });
+        });
+        await _context.SaveChangesAsync();
     }
 
     public async Task<List<Show>> GetAllAsync()
     {
         return await _context.Shows
-            .Include(show =>
-                show.ShowActors.Select(showActor => showActor.Actor).OrderBy(x => x.Birthday))
+            .Include(show => show.ShowRoles.OrderBy(x => x.Actor.Birthday))
+            .ThenInclude(role => role.Actor)
             .ToListAsync();
     }
 
     public async Task<long> GetLastUpdatedValue()
     {
-        return await _context.Shows.Select(x => x.LastUpdated).DefaultIfEmpty(0).MaxAsync();
+        long value;
+        try
+        {
+            value = await _context.Shows.Select(x => x.LastUpdated).MaxAsync();
+        }
+        catch (InvalidOperationException)
+        {
+            value = 0;
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError("Problem with {1}", ex.Message);
+            throw;
+        }
+        return value;
     }
 
     public async Task<List<Show>> GetPageAsync(int pageNumber = 0, int pageSize = 250)
