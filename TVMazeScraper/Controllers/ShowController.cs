daa3a0089158a46e9a6429a5ee4d2e5dffc3a8af
Dan <decisivedaniel@proton.me>
2025-06-01 15:59:53 +0200

    Validation on get requests

--- TVMazeScraper/Controllers/ShowController.cs
+++ TVMazeScraper/Controllers/ShowController.cs
@@ -8,6 +8,8 @@ namespace TVMazeScraper.Controllers;
 [Route("[controller]")]
 public class ShowController : ControllerBase
 {
+    private readonly int PageMin = 0;
+    private readonly int PageAmountMin = 50;
     private readonly ILogger<ShowController> _logger;
     private readonly IShowService _service;
 
@@ -17,9 +19,14 @@ public class ShowController : ControllerBase
         _logger = logger;
     }
 
-    [HttpGet(Name = "getshows/{page?}/{amount?}")]
+    [HttpGet("{page?}/{amount?}", Name = "Show_Pages")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status204NoContent)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
     public async Task<ActionResult<IEnumerable<ShowDTO>>> GetShows(int page = 0, int amount = 100)
     {
+        if (page < PageMin) return BadRequest($"Page number can't be below {PageMin}");
+        if (amount < PageAmountMin) return BadRequest($"Keep each page to at least {PageAmountMin}");
         var shows = await _service.GetPageAsync(page, amount);
         if (shows.Count == 0) return NoContent();
         return shows.Select(s => new ShowDTO(s)).ToList();
